8f58bb9fd4ced9431f4bbc617bdf7b5911a8106c
shady356 <heenriko@gmail.com>
2022-01-03 00:50:16 +0100

    feat: init map

---
+++ src/components/Game.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Game</h1>
+    <h2>Select Norway</h2>
+    <div class="map">
+      <MapEurope class="map--image" @click="handle" />
+    </div>
+    <pre>{{ selected }}</pre>
+  </div>
+</template>
+
+<script>
+import MapEurope from "@/components/MapEurope.vue";
+export default {
+  components: {
+    MapEurope,
+  },
+  data() {
+    return {
+      selected: null,
+    };
+  },
+  methods: {
+    handle(e) {
+      this.selected = e.target.id;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.map {
+  max-width: 900px;
+  .map--image {
+    width: 100%;
+  }
+}
+</style>
